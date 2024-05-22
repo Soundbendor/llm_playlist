04b26acb02a7b5744b09ca720dd153f1c2d5a0a2
Derek Kwan <dkwan6@gatech.edu>
2024-05-22 16:34:29 -0700

    adding index

--- playlist_binner.py
+++ playlist_binner.py
@@ -10,9 +10,9 @@ track_bins = {x: [] for x in range(50,450,50)}
 if os.path.exists(data_path) == False:
     os.mkdir(data_path)
 
-outkeys = ['name', 'num_tracks', 'file', 'pid', 'modified_at', 'collaborative', 'num_albums', 'num_followers']
+outkeys = ['name', 'num_tracks', 'idx', 'file', 'pid', 'modified_at', 'collaborative', 'num_albums', 'num_followers']
 
-def get_playlist_info(cur_file, playlist):
+def get_playlist_info(cur_file, playlist, pl_idx):
     cur_name = playlist['name']
     cur_collab = playlist['collaborative']
     cur_pid = playlist['pid']
@@ -20,7 +20,7 @@ def get_playlist_info(cur_file, playlist):
     cur_albums = playlist['num_albums']
     cur_follow = playlist['num_followers']
     cur_mod = playlist['modified_at']
-    return {'name': cur_name, 'file': cur_file, 'pid': cur_pid, 'num_tracks': cur_tracks,
+    return {'name': cur_name, 'idx': pl_idx, 'file': cur_file, 'pid': cur_pid, 'num_tracks': cur_tracks,
             'modified_at': cur_mod, 'collaborative': cur_collab, 'num_albums': cur_albums,
             'num_followers': cur_follow}
 
@@ -31,8 +31,6 @@ def get_bin(playlist):
 
 for i,mpdslice in enumerate(os.listdir(datadir)):
     cpath = os.path.join(datadir, mpdslice)
-    if i > 0:
-        break
     nums = mpdslice.split('.')[-2]
     local_track = Counter()
     local_album = Counter()
@@ -40,10 +38,10 @@ for i,mpdslice in enumerate(os.listdir(datadir)):
     with open(cpath, 'r') as f:
         j = json.load(f)
         #print(j.keys())
-        for playlist in j['playlists']:
+        for pl_idx, playlist in enumerate(j['playlists']):
             #print(playlist['num_tracks'])
             cur_bin = get_bin(playlist)
-            cur_info = get_playlist_info(mpdslice,playlist)
+            cur_info = get_playlist_info(mpdslice,playlist, pl_idx)
             if cur_bin not in track_bins.keys():
                 track_bins[cur_bin] = []
             track_bins[cur_bin].append((playlist['num_tracks'], playlist['pid'], cur_info))
