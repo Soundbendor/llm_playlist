f63ed20d76193a13cea2135c24d398d65de36605
Derek Kwan <dkwan6@gatech.edu>
2024-05-22 22:52:04 -0700

    adding feature getter

--- util/getter.py
+++ util/getter.py
@@ -1,14 +1,18 @@
 from collections import Counter
 import matplotlib.pyplot as plt
-import os, json,csv
+import os, json,csv,sqlite3
 import numpy as np
+import glob as G
 
+nct_feat = ["track_name", "artist_name", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "type", "id", "uri", "track_href", "analysis_url", "duration_ms", "time_signature"]
 
-datadir = "/media/dxk/TOSHIBA EXT/ds/spotify_mpd/data"
-data_path = os.path.join(datadir, 'data')
+# new combined table tup to dictionary when using select *
+def nct_tup_to_dict(nct_tup):
+    return {x:y for (x,y) in zip(nct_feat, nct_tup)}
+
 
 def get_playlist(file, idx):
-    cur_path = os.path.join(datadir, file)
+    cur_path = os.path.join(G.data_dir, file)
     res = None
     with open(cur_path, 'r') as f:
         j = json.load(f)
@@ -17,11 +21,34 @@ def get_playlist(file, idx):
 
     return res
 
+# connect to new combined table
+def connect_to_nct():
+    cnx = sqlite3.connect(G.joined_db_path)
+    cur = cnx.cursor()
+    return cnx, cur
+
+def get_features_by_id(cur,_id):
+    res = cur.execute(f'SELECT * FROM new_combined_table WHERE id="{_id}"')
+    restup = res.fetchone()
+    resdict = None
+    try:
+        resdict = nct_tup_to_dict(restup)
+    except:
+        resdict = {}
+    return resdict
+
+
 if __name__ == "__main__":
     res = get_playlist('mpd.slice.549000-549999.json', 333)
-    print(res)
+    #print(res)
     res2 = get_playlist('mpd.slice.549000-549999.json', 793)
-    print(res2)
+    #print(res2)
+    cnx, cur = connect_to_nct()
+    resdict = get_features_by_id(cur, "6JHrzpRYiDx53iTgTbI76X")
+    resdict2 = get_features_by_id(cur, "2Viqjkxmiu8hGIhjwtqYvI")
+    #print(resdict)
+    #print(resdict2)
+
 
 
 
---
+++ util/glob.py
@@ -0,0 +1,7 @@
+import os
+
+db_dir = "/media/dxk/TOSHIBA EXT/ds/spotify_track_features"
+data_dir = "/media/dxk/TOSHIBA EXT/ds/spotify_mpd/data"
+data_path = os.path.join(data_dir, 'data')
+joined_db_path = os.path.join(db_dir, "joined.db")
+
