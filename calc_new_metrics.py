32b5f2777d5df6a1c4a867f70f82584c7a64742d
John Zontos <[jczontos@gmail.com]>
2024-05-30 09:27:40 -0700

    optimized popularity metrics code

--- calc_new_metrics.py
+++ calc_new_metrics.py
@@ -2,7 +2,6 @@ import os
 import csv
 import json
 import glob
-import time
 from collections import Counter
 from tqdm import tqdm
 
@@ -14,6 +13,18 @@ all_files = set(glob.glob(file_pattern)) - set(glob.glob(os.path.join(G.num_trac
 
 ctr = Counter()
 
+# Load JSON data in advance if possible
+playlist_data = {}
+def load_playlist(plpath):
+    if plpath not in playlist_data:
+        try:
+            with open(plpath, 'r') as f:
+                playlist_data[plpath] = json.load(f)
+        except Exception as e:
+            print(f"Error loading {plpath}: {e}")
+            playlist_data[plpath] = None
+    return playlist_data[plpath]
+
 for cur_path in tqdm(all_files, desc="Processing files"):
     with open(cur_path, 'r') as file:
         csvr = csv.DictReader(file)
@@ -22,25 +33,16 @@ for cur_path in tqdm(all_files, desc="Processing files"):
             plfile = row['file']
             plidx = int(row['idx'])
             plpath = os.path.join(G.data_dir, plfile)
-            try:
-                with open(plpath, 'r') as f:
-                    cur_json = json.load(f)
-                    cur_pl = cur_json['playlists'][plidx]
-                    trk_ids = [trk['track_uri'] for trk in cur_pl['tracks']]
-                    ctr.update(trk_ids)
-            except json.JSONDecodeive:
-                print(f"Error decoding JSON from {plpath}")
-            except FileNotFoundError:
-                print(f"File not found: {plpath}")
-            except Exception as e:
-                print(f"Unexpected error: {e}, file: {plpath}")
-            
-            # Logging
+            cur_json = load_playlist(plpath)
+            if cur_json is None:
+                continue
+
+            cur_pl = cur_json['playlists'][plidx]
+            trk_ids = [trk['track_uri'] for trk in cur_pl['tracks']]
+            ctr.update(trk_ids)
             i+=1
-            if i % 100 == 0:
-                print(i/100)
-                print(f"time:  {time.time()}")
-                # break
+            if i % 1000 == 0:
+                print(i)
 
 # Write the popularity counts to file
 output_file = os.path.join(G.num_tracks_path, 'popularity.csv')
--- job.sh
+++ job.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=llm_playlist
+#SBATCH -w cn-d-1
+#SBATCH -p preempt
+#SBATCH --job-name=popularity_metrics
 #SBATCH -t 4-00:00:00
 #SBATCH -c 1
 #SBATCH --mem=90G
@@ -8,4 +10,4 @@
 # activate env
 source env/bin/activate
 
-python post_llm.py
+python calc_new_metrics.py
