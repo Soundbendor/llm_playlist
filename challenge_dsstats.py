670a0e1974edfa6ed52ffaee02453270822ae7a8
Derek Kwan <dkwan6@gatech.edu>
2024-06-19 01:54:01 -0700

    adding challenge set stats

---
+++ challenge_dsstats.py
@@ -0,0 +1,64 @@
+import csv,json,os
+import routes as G
+import getter as UG
+import numpy as np
+cj = None
+
+# max lengths conditioning on...
+# 0: 50, 1: 78, < 0.5: 199 (0.49749), >= 0.5: 250 (0.90000)
+
+
+out_path = os.path.join(os.sep.join(__file__.split(os.sep)[:-1]), 'data')
+out_file = os.path.join(out_path, 'challenge_set_stats.csv')
+with open(G.challenge_file, 'r') as f:
+    cj = json.load(f)
+
+header = ['name', 'pid', 'num_tracks', 'num_holdouts', 'holdout_ratio']
+max_len_cond_1 = -1
+max_len_cond_0 = -1
+max_len_cond_lt50 = -1
+max_r_lt50 = -1
+max_r_gt50 = -1
+max_len_cond_gt50 = -1
+with open(out_file, 'w') as f:
+    csvw = csv.DictWriter(f, fieldnames=header)
+    csvw.writeheader()
+    for pl in cj['playlists']:
+        name = ''
+        nt = pl['num_tracks']
+        nh = pl['num_holdouts']
+        nc = nt - nh
+        r = float(nh)/float(nt)
+        if nc == 0:
+            max_len_cond_0 = max(max_len_cond_0, nt)
+        elif nc == 1:
+            max_len_cond_1 = max(max_len_cond_1, nt)
+        elif r < 0.5:
+            if nt > max_len_cond_lt50:
+                max_len_cond_lt50 = nt
+                max_r_lt50 = r
+        else:
+            if nt > max_len_cond_gt50:
+                max_len_cond_gt50 = nt
+                max_r_gt50 = r
+ 
+
+        if 'name' in pl.keys():
+            name = pl['name']
+        d = {'name': name, 'pid': pl['pid'],
+             'num_tracks': nt, 
+             'num_holdouts': nh,
+             'holdout_ratio': r
+             }
+        csvw.writerow(d)
+
+print('max lengths conditioning on...')
+print(f'0: {max_len_cond_0}, 1: {max_len_cond_1}, < 0.5: {max_len_cond_lt50} ({max_r_lt50:.5f}), >= 0.5: {max_len_cond_gt50} ({max_r_gt50:.5f})')
+
+
+
+
+
+
+
+
