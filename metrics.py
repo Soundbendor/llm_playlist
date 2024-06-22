f166f57340fb9fb5f22eea3fd9fcc180137ba071
Derek Kwan <dkwan6@gatech.edu>
2024-06-22 15:47:07 -0700

    add precision

--- metrics.py
+++ metrics.py
@@ -104,16 +104,23 @@ def reciprocal_rank(g_arr, r_arr):
 
     return ret
 
+def precision(g_arr, r_arr):
+    g_in_r = np.isin(g_arr, r_arr)
+    return np.mean(g_in_r)
+
 if __name__ == "__main__":
     p1 = np.array(['spotify:track:4vv1KjUzPwrtDbozizSfQc', 'spotify:track:0Ws8D3EWUDgY962Xftb0h5', 'spotify:track:0lMbuWUpfTWhEmOKxppEau', 'spotify:track:4KaIJ1FWXUoAAnOts1YWjD', 'spotify:track:60APt5N2NRaKWf5xzJdzyC', 'spotify:track:4c1BAfuPGZSun6aAvmmoHs', 'spotify:track:25oOaleife6E2MIKmFkPvg', 'spotify:track:1UAmQe8EwpxQ80OfYVD13z', 'spotify:track:2e3OgIbfZw5deCjLMGatSS', 'spotify:track:5de7ci7TFqbQ1PFgKAD7MR', 'spotify:track:29BXCsh4lGLrndprkgYL6O', 'spotify:track:1jQsKN68yE94tMYml0wHMd', 'spotify:track:59J5nzL1KniFHnU120dQzt', 'spotify:track:1e1JKLEDKP7hEQzJfNAgPl', 'spotify:track:5CtI0qwDJkDQGwXD1H1cLb', 'spotify:track:152lZdxL1OR0ZMW6KquMif', 'spotify:track:3DXncPQOG4VBw3QHh3S817', 'spotify:track:2of5xn0GU0TdFneR1saRLH', 'spotify:track:2INqEk4ko5AsGVLBsiKiQe', 'spotify:track:3LRddJIw2ymm1CHIO9xlkC', 'spotify:track:4uTTsXhygWzSjUxXLHZ4HW', 'spotify:track:1jNyxG5S2P9gztbfAnrq85', 'spotify:track:4HW5kSQ8M2IQWZhSxERvla', 'spotify:track:3nVDOYBJpdCkRR6r1DbZum', 'spotify:track:1rsAFUCa6BVMgRQ3FCQQsi', 'spotify:track:6kig1UFggPUyZBCvXD3Wod'])
     p2 = p1.copy()
+    #p2 = np.concatenate((p1,p1))
     p1len = p1.shape[0]
-    p1idx = np.arange(0,p1len)
-    p2[0:3] = 'spotify:track:4vv1KjUzPwrtDbozizSfQd'
+    #p1idx = np.arange(0,p1len)
+    p2[0:5] = 'spotify:track:4vv1KjUzPwrtDbozizSfQd'
     _dcg = ndcg(p1,p1[::-2])
     _rr = reciprocal_rank(p1, p2)
+    _pr = precision(p1, p2)
     #print(p1)
     print(_rr)
+    print(_pr)
     #print(p1[::-2])
     #print(_dcg)
 
